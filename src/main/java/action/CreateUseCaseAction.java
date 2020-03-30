dc77471b17388da17625e4ac1e5c7817f093be55
MeePwn <dong.chunnan@chinamworld.com>
2020-03-31 00:21:58 +0800

    first commit

--- src/main/java/action/CreateUseCaseAction.java
+++ src/main/java/action/CreateUseCaseAction.java
@@ -5,9 +5,14 @@ import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.samskivert.mustache.Mustache;
+import com.samskivert.mustache.Template;
 import constant.Constant;
 import org.jetbrains.annotations.NotNull;
 import utils.ProjectUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateUseCaseAction extends AnAction {
 
@@ -15,7 +20,13 @@ public class CreateUseCaseAction extends AnAction {
   public void actionPerformed(@NotNull AnActionEvent e) {
     String rootPath = ProjectUtils.getRootPath(e);
 
+    String targetString = "== {{name}} ==";
+    Template template = Mustache.compiler().compile(targetString);
+    Map<String, String> data = new HashMap();
+    data.put("name", "zhangsan");
+    String result = template.execute(data);
+
     Notifications.Bus.notify(
-        new Notification(Constant.PLUGIN_TAG, "文件信息", rootPath, NotificationType.INFORMATION));
+        new Notification(Constant.PLUGIN_TAG, result, rootPath, NotificationType.INFORMATION));
   }
 }
